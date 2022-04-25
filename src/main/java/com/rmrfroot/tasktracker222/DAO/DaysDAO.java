e47a8a42444a5b2043da92909eaca908b9f8614a
tmaduchukwu <tmaduchukwu22@gmail.com>
2022-04-25 16:17:45 -0700

    Implemented an Interface for edits to database added initial method headers in Day.java for CRUD. Also added a database connection to execute the statements (may not be needed).

--- src/main/java/com/rmrfroot/tasktracker222/DAO/DaysDAO.java
+++ src/main/java/com/rmrfroot/tasktracker222/DAO/DaysDAO.java
@@ -3,6 +3,16 @@ package com.rmrfroot.tasktracker222.DAO;
 import com.rmrfroot.tasktracker222.Entity.Day;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.Optional;
+
 
 public interface DaysDAO extends JpaRepository<Day,Integer> {
+    public interface Dao<T, I> {
+        Optional<T> get(int id);
+        Collection<T> getAll();
+        Optional<I> save(T t);
+        void update(T t);
+        void delete(T t);
+    }
 }
--- src/main/java/com/rmrfroot/tasktracker222/Entity/Day.java
+++ src/main/java/com/rmrfroot/tasktracker222/Entity/Day.java
@@ -1,11 +1,22 @@
 package com.rmrfroot.tasktracker222.Entity;
 
 
+import com.rmrfroot.tasktracker222.DAO.DaysDAO;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
 import javax.persistence.*;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Entity
 @Table(name="days")
-public class Day {
+public class Day implements DaysDAO {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -74,6 +85,21 @@ public class Day {
         this.email = email;
     }
 
+    //updates
+//    public String deleteEmployeeById(int id) throws SQLException {
+//        getDBConnection();
+//        stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+//                ResultSet.CONCUR_READ_ONLY);
+//        sqlString = "DELETE FROM Employees WHERE employee_id = " + id;
+//        System.out.println("\nExecuting: " + sqlString);
+//        stmt.execute(sqlString);
+//        return "success";
+//    }
+
+    private void getDBConnection() {
+    }
+
+
     @Override
     public String toString() {
         return "account{" +
@@ -83,5 +109,150 @@ public class Day {
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
                 '}';
+    }
+
+    @Override
+    public List<Day> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Day> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Day> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Day> findAllById(Iterable<Integer> integers) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(Day entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Integer> integers) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Day> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends Day> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Day> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Day> findById(Integer integer) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Day> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Day> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<Day> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Integer> integers) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Day getOne(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Day getById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public <S extends Day> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Day> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Day> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Day> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Day> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Day> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends Day, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
     }
 }
