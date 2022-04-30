c2c12a6f68a0285d165524ae5b544483783c8a70
Anish Gautam <82066251+CrunchWrapSuprmeCommits@users.noreply.github.com>
2022-04-30 16:17:51 -0700

    added dayCollection entity class

--- src/main/java/com/rmrfroot/tasktracker222/Entity/DayCollection.java
+++ src/main/java/com/rmrfroot/tasktracker222/Entity/DayCollection.java
@@ -1,4 +1,75 @@
 package com.rmrfroot.tasktracker222.Entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "drill_schedule")
 public class DayCollection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String event_title;
+    private String start_date;
+    private String location;
+    private String officer_name;
+    private String officer_email;
+    private String note;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEvent_title() {
+        return event_title;
+    }
+
+    public void setEvent_title(String event_title) {
+        this.event_title = event_title;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOfficer_name() {
+        return officer_name;
+    }
+
+    public void setOfficer_name(String officer_name) {
+        this.officer_name = officer_name;
+    }
+
+    public String getOfficer_email() {
+        return officer_email;
+    }
+
+    public void setOfficer_email(String officer_email) {
+        this.officer_email = officer_email;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
 }
---
+++ src/main/java/com/rmrfroot/tasktracker222/Repository/DayCollectionRepo.java
@@ -0,0 +1,7 @@
+package com.rmrfroot.tasktracker222.Repository;
+
+import com.rmrfroot.tasktracker222.Entity.DayCollection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class DayCollectionRepo extends JpaRepository<DayCollection, Integer> {
+}
