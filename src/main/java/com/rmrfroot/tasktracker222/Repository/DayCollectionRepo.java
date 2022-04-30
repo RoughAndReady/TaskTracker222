a10f747e774a9247f82395d2f8f90603bfc2848a
Anish Gautam <82066251+CrunchWrapSuprmeCommits@users.noreply.github.com>
2022-04-30 16:31:24 -0700

    deleted the working repository file, I'll push when it works

--- src/main/java/com/rmrfroot/tasktracker222/Repository/DayCollectionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.rmrfroot.tasktracker222.Repository;
-
-import com.rmrfroot.tasktracker222.Entity.DayCollection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class DayCollectionRepo extends JpaRepository<DayCollection, Integer> {
-}
