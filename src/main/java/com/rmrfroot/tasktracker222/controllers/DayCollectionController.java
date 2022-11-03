e52b74d0c3ce40e39a0536b4795e4e3c85f974d8
tmaduchukwu <tmaduchukwu22@gmail.com>
2022-11-02 21:55:42 -0700

    DT-241. Created a Javadoc comments for DayCollectionController.

--- src/main/java/com/rmrfroot/tasktracker222/controllers/DayCollectionController.java
+++ src/main/java/com/rmrfroot/tasktracker222/controllers/DayCollectionController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Day Collection Controller Class for Day Collection
+ * @author Brian
+ * @author Visoth
+ */
 @RestController
 @RequestMapping("/dayCollection")
 public class DayCollectionController {
@@ -19,6 +24,11 @@ public class DayCollectionController {
     private DaoService daoService;
 
 
+    /**
+     * Gets a colection of days and creates a model
+     * @param model
+     * @return dayCollection
+     */
     @GetMapping
     public String main(Model model){
         List<Day> dayList =daoService.findAll();
@@ -27,21 +37,41 @@ public class DayCollectionController {
         return "dayCollection";
     }
 
+    /**
+     * Search a Day object by an integer id
+     * @param id of day to be searched
+     * @return ResponseEntity with HTML response code 200
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Day> findById(@PathVariable("id") int id) {
         return new ResponseEntity<>(daoService.findById(id), HttpStatus.OK);
     }
 
+    /**
+     * Save a Day object to the database
+     * @param day to be saved
+     * @return ResponseEntity with HTML response code 200
+     */
     @PostMapping
     public ResponseEntity<Day> save(@RequestBody Day day) {
         return new ResponseEntity<>(daoService.save(day), HttpStatus.CREATED);
     }
 
+    /**
+     * Update day's attributes
+     * @param id of day to be updated
+     * @param day body to be updated
+     * @return ResponseEntity with HTML response code 200
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Day> update(@PathVariable("id") int id, @RequestBody Day day) {
         return new ResponseEntity<>(daoService.update(id, day), HttpStatus.OK);
     }
 
+    /**
+     * Deletes a day
+     * @param id of day to be removed
+     */
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") int id) {
         daoService.deleteById(id);
