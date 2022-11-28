b61379408ba8bf482f53b242f6f7b819aa0101ff
Amrin S <amrin.sandhar@gmail.com>
2022-11-27 17:18:46 -0800

    added support for new drill schedule front-end and concurrency checks

--- src/main/java/com/rmrfroot/tasktracker222/services/DrillDaoImpl.java
+++ src/main/java/com/rmrfroot/tasktracker222/services/DrillDaoImpl.java
@@ -3,10 +3,15 @@ package com.rmrfroot.tasktracker222.services;
 
 import com.rmrfroot.tasktracker222.dao.DrillDAO;
 import com.rmrfroot.tasktracker222.entities.Drill;
+import com.rmrfroot.tasktracker222.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
 
 /**
  * drill dao implementation
@@ -18,6 +23,9 @@ public class DrillDaoImpl implements DrillDaoService{
 
     @Autowired
     private DrillDAO drillDAO;
+
+    @Autowired
+    private UsersDaoService usersDaoService;
 
     /**
      * Finds all drills that are registered in the system
@@ -117,4 +125,194 @@ public class DrillDaoImpl implements DrillDaoService{
         return updatedDrill;
     }
 
+    public List<Drill> findDrillsInWeekOfDate(String date){
+        List<Drill> allDrills = drillDAO.findAll();
+        List<Drill> out = new ArrayList<>();
+
+        LocalDate startDate = findStartOfWeek(date);
+        LocalDate endDate = findEndOfWeek(date);
+
+        for (Drill drill : allDrills) {
+            LocalDate drillDate = getLocalDateOfDrill(drill);
+
+            if(drillDate.isBefore(endDate) && drillDate.isAfter(startDate)
+                    || drillDate.isEqual(startDate) || drillDate.isEqual(endDate)){
+                out.add(drill);
+            }
+        }
+
+        return out;
+    }
+
+    public List<Drill> findDrillsInWeekOfDateByID(String date, int id){
+        List<Drill> allDrills = findDrillsInWeekOfDate(date);
+        List<Drill> out = new ArrayList<>();
+
+        User user = usersDaoService.findUsersById(id);
+
+        for (Drill drill : allDrills) {
+            List<String> participants = drill.getParticipants();
+
+
+
+            if(participants.contains(Integer.toString(user.getId()))){
+                out.add(drill);
+            }
+            else{
+                List<String> commonGroups = new ArrayList<>(drill.getParticipants());
+
+                List<String> userGroups = new ArrayList<>();
+                userGroups.add(user.getRank());
+                userGroups.add(user.getFlight());
+                userGroups.add(user.getWorkCenter());
+                userGroups.addAll(user.getTeams());
+
+                commonGroups.retainAll(userGroups);
+
+                if(!commonGroups.isEmpty()){
+                    out.add(drill);
+                }
+            }
+        }
+
+        return out;
+    }
+
+    public LocalDate findStartOfWeek(String date){
+        LocalDate originalDate = convertStringToLocalDate(date);
+        LocalDate startDate = originalDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
+        return startDate;
+    }
+
+    public LocalDate findEndOfWeek(String date){
+        LocalDate originalDate = convertStringToLocalDate(date);
+        LocalDate endDate = originalDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
+        return endDate;
+    }
+
+    public int[] findDatesForScheduleByDate(String date){
+        int dates[] = new int[7];
+        int startDate = findStartOfWeek(date).getDayOfMonth();
+        int dateToAdd = startDate;
+
+        for(int i = 0; i < dates.length; i++){
+            dates[i] = dateToAdd;
+            dateToAdd++;
+        }
+
+        return dates;
+    }
+
+    public LocalDate convertStringToLocalDate(String date){
+        return LocalDate.of(Integer.parseInt(date.substring(0, 4)),
+                Integer.parseInt(date.substring(5,7)),
+                Integer.parseInt(date.substring(8,10)));
+    }
+
+    public LocalDate getLocalDateOfDrill(Drill drill){
+        return convertDateToLocalDate(drill.getDate());
+    }
+
+    public LocalDate convertDateToLocalDate(Date date){
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+
+    public Date combineDateAndTime(Date date, Date timeToFix){
+        timeToFix.setYear(date.getYear());
+        timeToFix.setMonth(date.getMonth());
+        timeToFix.setDate(date.getDate());
+
+        return timeToFix;
+    }
+
+    public String[] getDaysOfWeekWithConcurrency(LinkedHashMap<DayOfWeek, Integer> drills){
+        List<String> columns = new ArrayList<>();
+
+        drills.forEach((k,v) -> {
+            for(int i = 0; i <= v; i++){
+                columns.add(k.toString().substring(0,1) + k.toString().substring(1,3).toLowerCase()
+                        + (v > 0 ? "-" + i : ""));
+            }
+        });
+
+        return columns.toArray(new String[columns.size()]);
+    }
+
+    public LinkedHashMap<DayOfWeek, Integer> getDrillConcurrencyMap(List<Drill> drills){
+        LinkedHashMap<DayOfWeek, Integer> concurrency = new LinkedHashMap<>();
+        LinkedHashMap<DayOfWeek, List<Drill>> drillsByDay = sortDrillsByDayOfWeek(drills);
+
+        drillsByDay.forEach((k,v) -> {
+            concurrency.put(k, findConcurrencyInDay(v));
+        });
+
+        return concurrency;
+    }
+
+    public int findConcurrencyInDay(List<Drill> drills){
+        int concurrency = 0;
+
+        for (Drill drill : drills) {
+            int drillConcurrency = 0;
+            Date startTime = combineDateAndTime(drill.getDate(), drill.getStartTime());
+            Date endTime = combineDateAndTime(drill.getDate(), drill.getEndTime());
+
+            for (Drill drillToCompare : drills){
+                if(drill != drillToCompare) {
+                    Date startTimeCompare = combineDateAndTime(drillToCompare.getDate(), drillToCompare.getStartTime());
+                    Date endTimeCompare = combineDateAndTime(drillToCompare.getDate(), drillToCompare.getEndTime());
+
+                    if((startTime.before(endTimeCompare) || startTime.equals(endTimeCompare)) &&
+                            (startTimeCompare.before(endTime) || startTimeCompare.equals(endTime))) {
+                        drillConcurrency++;
+                    }
+                }
+            }
+
+            concurrency = Math.max(concurrency, drillConcurrency);
+        }
+
+        return concurrency;
+    }
+
+    public LinkedHashMap<DayOfWeek, List<Drill>> sortDrillsByDayOfWeek(List<Drill> drills){
+        LinkedHashMap<DayOfWeek, List<Drill>> drillsByDay = new LinkedHashMap<>();
+
+        List<Drill> drillsSunday = new ArrayList<>();
+        List<Drill> drillsMonday = new ArrayList<>();
+        List<Drill> drillsTuesday = new ArrayList<>();
+        List<Drill> drillsWednesday = new ArrayList<>();
+        List<Drill> drillsThursday = new ArrayList<>();
+        List<Drill> drillsFriday = new ArrayList<>();
+        List<Drill> drillsSaturday = new ArrayList<>();
+
+        for (Drill drill: drills) {
+            LocalDate drillDate = getLocalDateOfDrill(drill);
+
+            if(drillDate.getDayOfWeek() == DayOfWeek.SUNDAY)
+                drillsSunday.add(drill);
+            else if(drillDate.getDayOfWeek() == DayOfWeek.MONDAY)
+                drillsMonday.add(drill);
+            else if(drillDate.getDayOfWeek() == DayOfWeek.TUESDAY)
+                drillsTuesday.add(drill);
+            else if(drillDate.getDayOfWeek() == DayOfWeek.WEDNESDAY)
+                drillsWednesday.add(drill);
+            else if(drillDate.getDayOfWeek() == DayOfWeek.THURSDAY)
+                drillsThursday.add(drill);
+            else if(drillDate.getDayOfWeek() == DayOfWeek.FRIDAY)
+                drillsFriday.add(drill);
+            else if(drillDate.getDayOfWeek() == DayOfWeek.SATURDAY)
+                drillsSaturday.add(drill);
+        }
+
+        drillsByDay.put(DayOfWeek.SUNDAY, drillsSunday);
+        drillsByDay.put(DayOfWeek.MONDAY, drillsMonday);
+        drillsByDay.put(DayOfWeek.TUESDAY, drillsTuesday);
+        drillsByDay.put(DayOfWeek.WEDNESDAY, drillsWednesday);
+        drillsByDay.put(DayOfWeek.THURSDAY, drillsThursday);
+        drillsByDay.put(DayOfWeek.FRIDAY, drillsFriday);
+        drillsByDay.put(DayOfWeek.SATURDAY, drillsSaturday);
+
+        return drillsByDay;
+    }
 }
