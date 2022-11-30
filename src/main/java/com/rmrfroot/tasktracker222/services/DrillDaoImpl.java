1a7a59949d9817263a038065eb7bbdf4645e1dcc
Amrin S <amrin.sandhar@gmail.com>
2022-11-29 23:15:43 -0800

    fixed processing for date range generation, dates now roll over correctly into following month; no more 33rd of november

--- src/main/java/com/rmrfroot/tasktracker222/services/DrillDaoImpl.java
+++ src/main/java/com/rmrfroot/tasktracker222/services/DrillDaoImpl.java
@@ -10,16 +10,20 @@ import org.springframework.stereotype.Service;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * drill dao implementation
+ *
  * @author Brian Frey
  * @author Tobechi
  */
 @Service
-public class DrillDaoImpl implements DrillDaoService{
+public class DrillDaoImpl implements DrillDaoService {
 
     @Autowired
     private DrillDAO drillDAO;
@@ -29,8 +33,9 @@ public class DrillDaoImpl implements DrillDaoService{
 
     /**
      * Finds all drills that are registered in the system
-     * @author Brian Frey
+     *
      * @return all drills
+     * @author Brian Frey
      */
     @Override
     public List<Drill> findAll() {
@@ -39,9 +44,9 @@ public class DrillDaoImpl implements DrillDaoService{
 
     /**
      * function to find the drills by ID
-     * @param id
-     * there is data validation
-     * it prints if the ID number matches
+     *
+     * @param id there is data validation
+     *           it prints if the ID number matches
      * @throws RuntimeException if the id doesn't match it prints an error "Drill not find drill id ###"
      * @author Brian Frey
      */
@@ -49,10 +54,9 @@ public class DrillDaoImpl implements DrillDaoService{
     public Drill findById(int id) {
         Optional<Drill> result = drillDAO.findById(id);
         Drill d;
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             d = result.get();
-        }
-        else {
+        } else {
             return null;
         }
         return d;
@@ -60,6 +64,7 @@ public class DrillDaoImpl implements DrillDaoService{
 
     /**
      * function to save the drill
+     *
      * @author Brian Frey
      */
     @Override
@@ -70,21 +75,20 @@ public class DrillDaoImpl implements DrillDaoService{
 
     /**
      * function to delete a drill by their id
-     * @author Brian Frey
-     * @param id
-     * there is a data validation
-     * if the id matches, then the drill will delete
+     *
+     * @param id there is a data validation
+     *           if the id matches, then the drill will delete
      * @throws RuntimeException if the id doesn't match then it throws a exception
+     * @author Brian Frey
      */
     @Override
     public void deleteById(int id) {
         Optional<Drill> result = drillDAO.findById(id);
         Drill d = null;
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             d = result.get();
             drillDAO.deleteById(id);
-        }
-        else {
+        } else {
             //drill not found
             throw new RuntimeException("Did not find drill id - " + id);
         }
@@ -94,21 +98,21 @@ public class DrillDaoImpl implements DrillDaoService{
 
     /**
      * function to update the drill
-     * @author Brian Frey
+     *
      * @param id int and the updated drill
-     * there is data validation
+     *           there is data validation
      * @return if id matches then it goes ahead and updates the drill
      * @throws RuntimeException if the id doesn't match then throws and exception
+     * @author Brian Frey
      */
     @Override
     public Drill update(int id, Drill drill) {
         Optional<Drill> result = drillDAO.findById(id);
         Drill updatedDrill;
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             updatedDrill = result.get();
 //            drillDAO.deleteById(id);
-        }
-        else {
+        } else {
             //drill not found
             throw new RuntimeException("Did not find drill id - " + id);
         }
@@ -125,7 +129,7 @@ public class DrillDaoImpl implements DrillDaoService{
         return updatedDrill;
     }
 
-    public List<Drill> findDrillsInWeekOfDate(String date){
+    public List<Drill> findDrillsInWeekOfDate(String date) {
         List<Drill> allDrills = drillDAO.findAll();
         List<Drill> out = new ArrayList<>();
 
@@ -135,8 +139,8 @@ public class DrillDaoImpl implements DrillDaoService{
         for (Drill drill : allDrills) {
             LocalDate drillDate = getLocalDateOfDrill(drill);
 
-            if(drillDate.isBefore(endDate) && drillDate.isAfter(startDate)
-                    || drillDate.isEqual(startDate) || drillDate.isEqual(endDate)){
+            if (drillDate.isBefore(endDate) && drillDate.isAfter(startDate)
+                    || drillDate.isEqual(startDate) || drillDate.isEqual(endDate)) {
                 out.add(drill);
             }
         }
@@ -144,7 +148,7 @@ public class DrillDaoImpl implements DrillDaoService{
         return out;
     }
 
-    public List<Drill> findDrillsInWeekOfDateByID(String date, int id){
+    public List<Drill> findDrillsInWeekOfDateByID(String date, int id) {
         List<Drill> allDrills = findDrillsInWeekOfDate(date);
         List<Drill> out = new ArrayList<>();
 
@@ -154,11 +158,9 @@ public class DrillDaoImpl implements DrillDaoService{
             List<String> participants = drill.getParticipants();
 
 
-
-            if(participants.contains(Integer.toString(user.getId()))){
+            if (participants.contains(Integer.toString(user.getId()))) {
                 out.add(drill);
-            }
-            else{
+            } else {
                 List<String> commonGroups = new ArrayList<>(drill.getParticipants());
 
                 List<String> userGroups = new ArrayList<>();
@@ -169,7 +171,7 @@ public class DrillDaoImpl implements DrillDaoService{
 
                 commonGroups.retainAll(userGroups);
 
-                if(!commonGroups.isEmpty()){
+                if (!commonGroups.isEmpty()) {
                     out.add(drill);
                 }
             }
@@ -178,46 +180,50 @@ public class DrillDaoImpl implements DrillDaoService{
         return out;
     }
 
-    public LocalDate findStartOfWeek(String date){
+    public LocalDate findStartOfWeek(String date) {
         LocalDate originalDate = convertStringToLocalDate(date);
         LocalDate startDate = originalDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
         return startDate;
     }
 
-    public LocalDate findEndOfWeek(String date){
+    public LocalDate findEndOfWeek(String date) {
         LocalDate originalDate = convertStringToLocalDate(date);
         LocalDate endDate = originalDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
         return endDate;
     }
 
-    public int[] findDatesForScheduleByDate(String date){
-        int dates[] = new int[7];
-        int startDate = findStartOfWeek(date).getDayOfMonth();
-        int dateToAdd = startDate;
+    public int[] findDatesForScheduleByDate(String date) {
+        ArrayList<Integer> dates = new ArrayList<>();
 
-        for(int i = 0; i < dates.length; i++){
-            dates[i] = dateToAdd;
-            dateToAdd++;
+        LocalDate startDate = findStartOfWeek(date);
+        LocalDate endDate = findEndOfWeek(date);
+
+        List<LocalDate> datesToAdd = startDate.datesUntil(endDate).collect(Collectors.toList());
+
+        for(LocalDate d : datesToAdd) {
+            dates.add(d.getDayOfMonth());
+            System.out.println(d.getDayOfMonth());
         }
+        dates.add(endDate.getDayOfMonth()); //LocalDate.datesUntil() is not inclusive, add final date
 
-        return dates;
+        return dates.stream().mapToInt(i->i).toArray();
     }
 
-    public LocalDate convertStringToLocalDate(String date){
+    public LocalDate convertStringToLocalDate(String date) {
         return LocalDate.of(Integer.parseInt(date.substring(0, 4)),
-                Integer.parseInt(date.substring(5,7)),
-                Integer.parseInt(date.substring(8,10)));
+                Integer.parseInt(date.substring(5, 7)),
+                Integer.parseInt(date.substring(8, 10)));
     }
 
-    public LocalDate getLocalDateOfDrill(Drill drill){
+    public LocalDate getLocalDateOfDrill(Drill drill) {
         return convertDateToLocalDate(drill.getDate());
     }
 
-    public LocalDate convertDateToLocalDate(Date date){
+    public LocalDate convertDateToLocalDate(Date date) {
         return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
-    public Date combineDateAndTime(Date date, Date timeToFix){
+    public Date combineDateAndTime(Date date, Date timeToFix) {
         timeToFix.setYear(date.getYear());
         timeToFix.setMonth(date.getMonth());
         timeToFix.setDate(date.getDate());
@@ -225,12 +231,12 @@ public class DrillDaoImpl implements DrillDaoService{
         return timeToFix;
     }
 
-    public String[] getDaysOfWeekWithConcurrency(LinkedHashMap<DayOfWeek, Integer> drills){
+    public String[] getDaysOfWeekWithConcurrency(LinkedHashMap<DayOfWeek, Integer> drills) {
         List<String> columns = new ArrayList<>();
 
-        drills.forEach((k,v) -> {
-            for(int i = 0; i <= v; i++){
-                columns.add(k.toString().substring(0,1) + k.toString().substring(1,3).toLowerCase()
+        drills.forEach((k, v) -> {
+            for (int i = 0; i <= v; i++) {
+                columns.add(k.toString().substring(0, 1) + k.toString().substring(1, 3).toLowerCase()
                         + (v > 0 ? "-" + i : ""));
             }
         });
@@ -238,18 +244,18 @@ public class DrillDaoImpl implements DrillDaoService{
         return columns.toArray(new String[columns.size()]);
     }
 
-    public LinkedHashMap<DayOfWeek, Integer> getDrillConcurrencyMap(List<Drill> drills){
+    public LinkedHashMap<DayOfWeek, Integer> getDrillConcurrencyMap(List<Drill> drills) {
         LinkedHashMap<DayOfWeek, Integer> concurrency = new LinkedHashMap<>();
         LinkedHashMap<DayOfWeek, List<Drill>> drillsByDay = sortDrillsByDayOfWeek(drills);
 
-        drillsByDay.forEach((k,v) -> {
+        drillsByDay.forEach((k, v) -> {
             concurrency.put(k, findConcurrencyInDay(v));
         });
 
         return concurrency;
     }
 
-    public int findConcurrencyInDay(List<Drill> drills){
+    public int findConcurrencyInDay(List<Drill> drills) {
         int concurrency = 0;
 
         for (Drill drill : drills) {
@@ -257,12 +263,12 @@ public class DrillDaoImpl implements DrillDaoService{
             Date startTime = combineDateAndTime(drill.getDate(), drill.getStartTime());
             Date endTime = combineDateAndTime(drill.getDate(), drill.getEndTime());
 
-            for (Drill drillToCompare : drills){
-                if(drill != drillToCompare) {
+            for (Drill drillToCompare : drills) {
+                if (drill != drillToCompare) {
                     Date startTimeCompare = combineDateAndTime(drillToCompare.getDate(), drillToCompare.getStartTime());
                     Date endTimeCompare = combineDateAndTime(drillToCompare.getDate(), drillToCompare.getEndTime());
 
-                    if((startTime.before(endTimeCompare) || startTime.equals(endTimeCompare)) &&
+                    if ((startTime.before(endTimeCompare) || startTime.equals(endTimeCompare)) &&
                             (startTimeCompare.before(endTime) || startTimeCompare.equals(endTime))) {
                         drillConcurrency++;
                     }
@@ -275,7 +281,7 @@ public class DrillDaoImpl implements DrillDaoService{
         return concurrency;
     }
 
-    public LinkedHashMap<DayOfWeek, List<Drill>> sortDrillsByDayOfWeek(List<Drill> drills){
+    public LinkedHashMap<DayOfWeek, List<Drill>> sortDrillsByDayOfWeek(List<Drill> drills) {
         LinkedHashMap<DayOfWeek, List<Drill>> drillsByDay = new LinkedHashMap<>();
 
         List<Drill> drillsSunday = new ArrayList<>();
@@ -286,22 +292,22 @@ public class DrillDaoImpl implements DrillDaoService{
         List<Drill> drillsFriday = new ArrayList<>();
         List<Drill> drillsSaturday = new ArrayList<>();
 
-        for (Drill drill: drills) {
+        for (Drill drill : drills) {
             LocalDate drillDate = getLocalDateOfDrill(drill);
 
-            if(drillDate.getDayOfWeek() == DayOfWeek.SUNDAY)
+            if (drillDate.getDayOfWeek() == DayOfWeek.SUNDAY)
                 drillsSunday.add(drill);
-            else if(drillDate.getDayOfWeek() == DayOfWeek.MONDAY)
+            else if (drillDate.getDayOfWeek() == DayOfWeek.MONDAY)
                 drillsMonday.add(drill);
-            else if(drillDate.getDayOfWeek() == DayOfWeek.TUESDAY)
+            else if (drillDate.getDayOfWeek() == DayOfWeek.TUESDAY)
                 drillsTuesday.add(drill);
-            else if(drillDate.getDayOfWeek() == DayOfWeek.WEDNESDAY)
+            else if (drillDate.getDayOfWeek() == DayOfWeek.WEDNESDAY)
                 drillsWednesday.add(drill);
-            else if(drillDate.getDayOfWeek() == DayOfWeek.THURSDAY)
+            else if (drillDate.getDayOfWeek() == DayOfWeek.THURSDAY)
                 drillsThursday.add(drill);
-            else if(drillDate.getDayOfWeek() == DayOfWeek.FRIDAY)
+            else if (drillDate.getDayOfWeek() == DayOfWeek.FRIDAY)
                 drillsFriday.add(drill);
-            else if(drillDate.getDayOfWeek() == DayOfWeek.SATURDAY)
+            else if (drillDate.getDayOfWeek() == DayOfWeek.SATURDAY)
                 drillsSaturday.add(drill);
         }
 
